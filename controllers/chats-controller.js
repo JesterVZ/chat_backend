79a8719541b706c8f041deeee2c1f13d772a553b
Jester <justjestervz@gmail.com>
2022-08-25 19:34:16 +0300

    get messages

--- controllers/chats-controller.js
+++ controllers/chats-controller.js
@@ -8,15 +8,11 @@ class ChatsController{
     
     async getChats(req, res){
         var body = req.body;
-        db.query('SELECT * FROM `chat_users` WHERE user_id = ?', [parseInt(body.user_id)], async(err, result) => { //
+        db.query('SELECT * FROM `chat_users` WHERE user_id = ?', [parseInt(body.user_id)], async(err, result) => {
             if(err){
                 return returnData(res, 300, err, null);
             }
-            res.statusCode = 200;
-            responseData["status-code"]=res.statusCode;
-            responseData["error"]=null;
-            responseData["data"] = result;
-            return res.json(responseData);
+            return returnData(res, 200, null, result);
         })
     }
     async createChat(req, res){ // работает, но не более 2-х, так как Cannot set headers after they are sent to the client
@@ -36,8 +32,7 @@ class ChatsController{
                             user_id: element
                         }, (error, result_users) => {
                             if(error){
-                                
-                                returnData(res, 300, error, null);
+                                return returnData(res, 300, error, null);
                             }else {
                                 resolve(result_users);
                             }
@@ -45,30 +40,35 @@ class ChatsController{
                         })
                     })
                 });
-                /*
-                for(var i = 0; i < body.user_id.length; i++){
-                    db.query('INSERT INTO `chat_users` SET ?', {
-                        chat_id: chatId,
-                        user_id: body.user_id[i]
-                    }, (error, result_users) => {
-                        if(error){
-                            
-                            returnData(res, 300, error, null);
-                        }
-                    })
-                    console.log(result_users);
-                }
-                */
-                //returnData(res, 200, null, "Чат успешно создан!");
                 return returnData(res, 200, null, "Чат успешно создан!");
             }
         })
     }
     async getMessages(req, res){
-
+        var body = req.body;
+        db.query('SELECT * FROM `messages` WHERE user_id = ?', [parseInt(body.user_id)], async(err, result) => {
+            if(err){
+                return returnData(res, 300, err, null);
+            }
+            return returnData(res, 200, null, result);
+        });
     }
     async sendMessage(req, res){
-
+        var body = req.body;
+        var currentdate = new Date(); 
+        const isoDate = new Date(currentdate);
+        db.query('INSERT INTO `messages` SET ?', {
+            user_id: body.user_id,
+            chat_id: body.chat_id,
+            message_text: body.message,
+            date_added: isoDate
+        }, (error, result) => {
+            if(error){
+                return returnData(res, 300, error, null);
+            } else {
+                return returnData(res, 200, null, result);
+            }
+        });
     }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,12 @@ import register from '../controllers/register.js';
 import ChatsController from '../controllers/chats-controller.js';
 import auth from '../controllers/auth.js'
 
-const router = express.Router()
+const router = express.Router();
 router.post('/reg', register);
 router.post('/login', login);
 router.post('/auth', auth);
-router.get('/get_chats', ChatsController.getChats)
-router.post('/create-chat', ChatsController.createChat)
+router.get('/get_chats', ChatsController.getChats);
+router.post('/create_chat', ChatsController.createChat);
+router.get('/get_messages', ChatsController.getMessages);
+router.post("/send_message", ChatsController.sendMessage);
 export default router
